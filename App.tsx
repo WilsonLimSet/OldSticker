15d75b4599cf0690bcb6191e556710fdf4d8795d
WilsonLimSet <limsetia@usc.edu>
2022-11-16 12:49:57 -0800

    cat 12pm

--- App.tsx
+++ App.tsx
@@ -3,6 +3,42 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, createContext, useContext, useEffect } from 'react';
+import TabOneScreen from './src/screens/TabOneScreen';
+
+const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
+
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  );
+};
+
+function ChatStack() {
+  return (
+    <Stack.Navigator defaultScreenOptions={TabOneScreen}>
+      <Stack.Screen name='Home' component={TabOneScreen} />
+      <Stack.Screen name='Chat' component={ModalScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Signup' component={Signup} />
+    </Stack.Navigator>
+  );
+}
 
 
 export default function App() {
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import Constants from "expo-constants";
-import firebase from 'firebase/app';
-import 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import Constants from 'expo-constants';
+// Firebase config
 const firebaseConfig = {
-  apiKey: Constants.manifest?.extra?.firebaseApiKey,
-  authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
-  projectId: Constants.manifest?.extra?.firebaseProjectId,
-  storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
-  messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
-  appId: Constants.manifest?.extra?.firebaseAppId
+  apiKey: Constants.manifest?.extra?.apiKey,
+  authDomain: Constants.manifest?.extra?.authDomain,
+  projectId: Constants.manifest?.extra?.projectId,
+  storageBucket: Constants.manifest?.extra?.storageBucket,
+  messagingSenderId: Constants.manifest?.extra?.messagingSenderId,
+  appId: Constants.manifest?.extra?.appId,
+  databaseURL: Constants.manifest?.extra?.databaseURL
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export default app;
+// initialize firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
