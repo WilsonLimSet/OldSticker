be01022668aa43dacdbae87c7455c3e7f2f8a54f
Sarah Liang <seliang@usc.edu>
2022-11-16 13:02:08 -0800

    small TS fixes

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './src/hooks/useCachedResources';
+import 'react-native-gesture-handler';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
-import Login from './screens/Login';
-import Signup from './screens/Signup';
+import Login from './src/screens/Login';
+import Signup from './src/screens/Signup';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { useState, createContext, useContext, useEffect } from 'react';
 import TabOneScreen from './src/screens/TabOneScreen';
+import ModalScreen from './src/screens/ModalScreen';
 
 const Stack = createStackNavigator();
 const AuthenticatedUserContext = createContext({});
 
-const AuthenticatedUserProvider = ({ children }) => {
+const AuthenticatedUserProvider = ({ children : any }) => {
   const [user, setUser] = useState(null);
 return (
     <AuthenticatedUserContext.Provider value={{ user, setUser }}>
